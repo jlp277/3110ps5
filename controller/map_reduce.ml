2cc393d91c2e314f2ff41f6f38873dfa01653a50
James Park <jlp277@cornell.edu>
2013-11-09 03:44:15 -0500

    attempt at integrating todo & map_pair. error in (unit -> unit) function

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -1,39 +1,34 @@
 open Util
 open Worker_manager
 
+let todo = Hashtbl.create 256
+let results = Hashtbl.create 256
 let c_mutex = Mutex.create ()
 
 (* TODO implement these *)
 let map kv_pairs map_filename : (string * string) list = 
   (* create threadpool *)
-  let to_combine = [] in
   let m_pool = Thread_pool.create 20 in
   let manager = Worker_manager.initialize_mappers map_filename in
-  let todo = kv_pairs in
-  (* create todo list *)
-  let map_pair (k,v) () =
-    let mapper = Worker_manager.pop_worker manager in
-    match Worker_manager.map mapper k v with
-    | Some l ->
-      Worker_manager.push_worker manager mapper;
-      Mutex.lock c_mutex;
-      l :: to_combine
-      (* check off todo *)
-      Mutex.unlock c_mutex;
-    | None -> 
-  
-  List.iter (fun (k,v) -> m_pool.add_work (map_pair (k,v)) ()) kv_pairs
-  (* iterate over todo list
-   head of list
-   Threadpool assign work
-
-   repeat *)
-  let rec todo_iter =
-    match todo with
-    | [] -> failwith "done"
-    | h::t ->
-      (* assign h to worker via thread *)
-  in
+  let rec assign =
+    let map_pair (k,v) () =
+      let mapper = Worker_manager.pop_worker manager in
+      match Worker_manager.map mapper k v with
+      | Some l ->
+        Worker_manager.push_worker manager mapper;
+        Mutex.lock c_mutex;
+        List.iter (fun (k,v) -> Hashtbl.add results k v) l;
+        Hashtbl.remove todo k;
+        Mutex.unlock c_mutex;
+      | None -> assign in
+    if Hashtbl.length todo = 0 then
+      Thread_pool.destroy m_pool
+    else
+      Thread.delay 0.1;
+      (* marshalling may not be type safe. check if this implementation is correct *)
+      Hashtbl.iter (fun (k,v) -> Thread_pool.add_work (map_pair (Util.marshal k, Util.marshal v) ()) m_pool) todo in
+  List.iter (fun (k,v) -> (Hashtbl.add todo k v)) kv_pairs;
+  assign
 
 let combine kv_pairs : (string * string list) list = 
   failwith "You have been doomed ever since you lost the ability to love."
