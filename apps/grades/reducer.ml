110a9a51b0e37cb0089b62fa15c181f2bc1d850d
James Park <jlp277@cornell.edu>
2013-11-14 19:42:05 -0500

    Fixes median grade reducer

--- apps/grades/reducer.ml
+++ apps/grades/reducer.ml
@@ -2,13 +2,14 @@ open Util;;
 
 let (key, values) = Program.get_input() in
 let get_median lst =
-  let len = List.length lst in
   let rec getnth n lst =
     match lst with
       | [] -> failwith "bad list index"
-      | h :: t -> if n = 1 then float_of_string h else getnth (n-1) t in
+      | h :: t -> if n = 1 then h else getnth (n-1) t in
+  let len = List.length lst in
   match len mod 2 with
-  | 0 -> ((getnth (len / 2) lst) +. (getnth (len / 2 + 1) lst)) /. 2.
-  | _ -> getnth (len / 2 + 2) lst in
-let median = get_median (List.sort compare values) in
+  | 0 -> ((getnth (len/2) lst)+.(getnth (len/2+1) lst))/.2.
+  | _ -> getnth (len/2+1) lst in
+let f_values = List.map (fun x -> float_of_string x) values in
+let median = get_median (List.sort compare f_values) in
 Program.set_output [string_of_float median]
